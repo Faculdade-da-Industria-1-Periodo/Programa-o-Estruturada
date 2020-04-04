15f8104f9b1be376f80293ec42dc3bfde82050a2
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2020-04-04 16:00:18 -0300

    Adicionado print exibindo o endereço da memória da variável e corrigido frase em outros prints

--- ponteiro/Ponteiro/main.c
+++ ponteiro/Ponteiro/main.c
@@ -12,6 +12,7 @@ int main()
 
     printf("\n******************\n");
     printf("Valor da variável: %d\n", valor);
-    printf("Valor do ponteiro: %d\n", ptr);
-    printf("Valor dentro do ponteiro: %d\n", *ptr);
+    printf("Endereço da variável na memória: %d\n", &valor);
+    printf("Valor da variável dentro do ponteiro: %d\n", *ptr);
+    printf("Endereço do ponteiro na memória: %d\n", ptr);
 }
