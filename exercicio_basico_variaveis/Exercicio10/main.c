ac2375b4a8f51cd5d24f7b468b99b6974c289443
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2020-04-04 15:04:53 -0300

    Código recebe um número fracionado e apresenta como inteiro

---
+++ exercicio_basico_variaveis/Exercicio10/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float numero;
+
+    printf("Digite um número fracionário: ");
+    scanf("%f", &numero);
+
+    printf("Número inteiro: %.0f\n", numero);
+
+    return 0;
+}
