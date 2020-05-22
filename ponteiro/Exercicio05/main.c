44784fd2c16d28328da299bc697c239f7c07526e
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2020-05-22 17:05:15 -0300

    Código que aloca duas structs, uma estática e outra dinâmica e faz o cadastro de produto na struct estática, após copia os dados para a struct dinâmica e imprime

---
+++ ponteiro/Exercicio05/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct PRODUTO_ESTATICO {
+    int quantidade;
+    char descricao[50];
+    float valor;
+};
+
+struct PRODUTO_DINAMICO {
+    int quantidade;
+    char descricao[50];
+    float valor;
+};
+
+int main()
+{
+    struct PRODUTO_ESTATICO estatico;
+    struct PRODUTO_DINAMICO *dinamico;
+
+    printf("----- Cadastro do produto -----\n");
+    printf("Quantidade: ");
+    scanf("%d", &estatico.quantidade);
+    getchar();
+    printf("Descrição: ");
+    scanf("%[^\n]s", &estatico.descricao);
+    getchar();
+    printf("Valor (R$): ");
+    scanf("%f", &estatico.valor);
+    getchar();
+
+    dinamico = (struct PRODUTO_DINAMICO*) malloc(sizeof(struct PRODUTO_DINAMICO));
+
+    dinamico->quantidade = estatico.quantidade;
+    strcpy(dinamico->descricao, estatico.descricao);
+    dinamico->valor = estatico.valor;
+
+    printf("\n----- Dados do Produto -----\n");
+    printf("Quantidade: %d\n", dinamico->quantidade);
+    printf("Descrição: %s\n", dinamico->descricao);
+    printf("Valor: R$%0.2f\n", dinamico->valor);
+
+    return 0;
+}
